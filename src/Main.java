3f94ae0e8127e37532f6f0c4d4b779ddb6572ab3
Seu Nome <melissa1996lima@gmail.com>
2026-04-15 22:06:30 -0300

    Correção e atualização do código.

--- src/Main.java
+++ src/Main.java
@@ -13,24 +13,24 @@ public class Main{
         Scanner leia = new Scanner(System.in);
 
         do {
-            System.out.println("\n[1] Saldo  [2] Depósito \n[3] Saque  [0] Sair");
-            System.out.println("\nEscolha uma opção: ");
+            System.out.println("\n************************\n[1] Saldo  [2] Depósito \n[3] Saque  [0] Sair");
+            System.out.print("\nEscolha uma opção: ");
             opcaoEscolhida = leia.nextInt();
 
             if (opcaoEscolhida < 0 || opcaoEscolhida > 3){
-                System.out.println("Opção inválida! Tente novamente.");
+                System.out.println("\nOpção inválida! Tente novamente.");
             }
             else  if (opcaoEscolhida == 0) {
-                System.out.println("Sistema encerrado!");
+                System.out.println("\nSistema encerrado!");
             }
             else {
                 switch (opcaoEscolhida) {
                     case 1:
-                        System.out.printf("Seu saldo atual é de: R$ %.2f%n", saldo);
+                        System.out.printf("\nSeu saldo atual é de: R$ %.2f%n", saldo);
                         break;
 
                     case 2:
-                        System.out.printf("Digite um valor para depositar: ");
+                        System.out.printf("\nDigite um valor para depositar: ");
                         double valorDeposito = leia.nextDouble();
                         if (valorDeposito > 0) {
                             saldo = saldo + valorDeposito;
@@ -46,11 +46,14 @@ public class Main{
                             System.out.println("\nVocê atingiu o limite máximo para saques diário!");
                         }
                         else {
-                            System.out.println("Digite um valor para saque: ");
+                            System.out.println("\nDigite um valor para saque: ");
                             double valorSaque = leia.nextDouble();
 
                             if (valorSaque > valorMxdeSaque) {
                                 System.out.println("\nSó é permitido saque até R$ 1000.00\n");
+                            }
+                            else if (valorSaque > saldo) {
+                                System.out.printf("\nSaldo insuficiente! Seu saldo atual é de R$ %.2f\n" , saldo);
                             }
                             else if (valorSaque <=0) {
                                 System.out.println("\nValor inválido!\n");
